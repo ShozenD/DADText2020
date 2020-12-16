31b06e1b0f9ce4be7c45efdf3bccaca54859a980
Shozen Dan <xiaoran.duan@gmail.com>
2020-12-16 17:38:07 +0900

    adding new data

--- notebooks/preprocess.py
+++ notebooks/preprocess.py
@@ -12,6 +12,7 @@
 #     name: python3
 # ---
 
+import re
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
@@ -21,6 +22,16 @@ uncy = pd.read_csv('../data/uncyclopedia.csv')
 wiki.head(10)
 
 uncy.head(10)
+
+
+# Remove new line character
+def parse(x):
+    x = re.sub("\n","", x)
+    return x
+
+
+wiki['Field1'] = wiki['Field1'].map(lambda x: parse(x))
+uncy['Field1'] = wiki['Field1'].map(lambda x: parse(x))
 
 wiki['label'] = 1
 uncy['label'] = 0
@@ -33,7 +44,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 train = pd.DataFrame({'label': y_train, 'text': X_train})
 test = pd.DataFrame({'label': y_test, 'text': X_test})
 
-test.to_csv('../data/test.txt', sep='\t', index=False, header=False)
-train.to_csv('../data/train.txt', sep='\t', index=False, header=False)
+test.to_csv('../data/test_orig.txt', sep='\t', index=False, header=False)
+train.to_csv('../data/train_orig.txt', sep='\t', index=False, header=False)
 
 
